3adcc5eef1bd2e2735ea6bf1ff24d081fb9a5e6b
MukPan <mushipan_mc@icloud.com>
2024-08-22 19:11:13 +0900

    fix: reqの型をNextRequestに変更してbuildできないバグを修正

--- src/app/api/v1/send-notification/route.ts
+++ src/app/api/v1/send-notification/route.ts
@@ -1,16 +1,17 @@
 // /api/v1/send-notification
+import {NextRequest} from "next/server";
 
-export async function GET(request: { url: string | URL }) {
-  const { searchParams } = new URL(request.url)
-  console.log("GET request", searchParams.get("name"))
-  return new Response(JSON.stringify({ message: "Hello World" }))
-}
-
-export async function POST(request: { json: () => any }) {
+export async function POST(request: NextRequest) {
   //bodyからデータを取得
   const body = await request.json();
   console.log(body);
 
-
   return new Response(JSON.stringify({ message: `Notification(${body.state}) has been sent.` }))
 }
+
+
+// export async function GET(request: { url: string | URL }) {
+//   const { searchParams } = new URL(request.url)
+//   console.log("GET request", searchParams.get("name"))
+//   return new Response(JSON.stringify({ message: "Hello World" }))
+// }
