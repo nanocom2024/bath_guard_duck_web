e2654bc953de61660b8efe8ce9607fa7289d1947
MukPan <mushipan_mc@icloud.com>
2024-08-22 19:01:16 +0900

    add: POSTリクエストの処理を追加

--- src/app/api/v1/send-notification/route.ts
+++ src/app/api/v1/send-notification/route.ts
@@ -1,13 +1,16 @@
+// /api/v1/send-notification
+
 export async function GET(request: { url: string | URL }) {
   const { searchParams } = new URL(request.url)
   console.log("GET request", searchParams.get("name"))
-
   return new Response(JSON.stringify({ message: "Hello World" }))
 }
 
 export async function POST(request: { json: () => any }) {
-  const body = await request.json()
+  //bodyからデータを取得
+  const body = await request.json();
+  console.log(body);
 
-  console.log("POST request", body.name)
-  return new Response(JSON.stringify({ message: "Hello World" }))
+
+  return new Response(JSON.stringify({ message: `Notification(${body.state}) has been sent.` }))
 }
