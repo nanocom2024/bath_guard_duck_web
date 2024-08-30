fbf200b6af21eeb483f5b1322915017c9bdcd70d
masaru-gif <k22089kk@aitech.ac.jp>
2024-08-30 18:13:14 +0900

    ログイン機能をhookにしました

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,12 +5,8 @@ import useFCM from "@/utils/hooks/useFCM"; // 通知送信用？
 import { getMessaging, onMessage } from "firebase/messaging"; // 通知送信用？
 import { NoticeMes } from "@/app/NoticeMes"; // 通知送信用？
 
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation'; // 画面遷移用
-import { auth } from '../../utils/firebase'; // farebaseの情報
-import { onAuthStateChanged, User } from 'firebase/auth'; // 認証確認用
-
-import useSaveToken from '@/utils/hooks/useSaveToken';
+import useSaveToken from '@/utils/hooks/useSaveToken'; // Token保存用
+import useAuthCheck from '@/utils/hooks/useAuthCheck'; // 認証確認用
 
 /* -----------------------------
 
@@ -21,25 +17,9 @@ import useSaveToken from '@/utils/hooks/useSaveToken';
 
 
 export default function Dashboard() {
-  const [user, setUser] = useState<User | null>(null); // ユーザーの状態
-  const router = useRouter(); // ルーターのフック
-
   const { messages, fcmToken } = useFCM();
-  const {isSaving, error } = useSaveToken(fcmToken);
-
-  useEffect(() => {
-    // ユーザーの認証状態を監視
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user); // 認証済みの場合はユーザー情報を設定
-      } else {
-        router.push('/login'); // 未認証の場合はログインページにリダイレクト
-      }
-    });
-
-    // クリーンアップ
-    return () => unsubscribe();
-  }, [router]);
+  useSaveToken(fcmToken); // tokenを保存
+  const user = useAuthCheck(); // 認証情報のcheck
 
   const messaging = getMessaging();
   onMessage(messaging, (payload) => {
@@ -50,6 +30,7 @@ export default function Dashboard() {
     <main className={styles.main}>
       <NoticeMes />
       <p>FCM Token: {fcmToken}</p>
+      <p>{user?.email}としてログイン中</p>
       <h1> 子供用ダック </h1>
       <Image
         src={"/images/duck.png"}
