c014810cbfb67d2c02290549952701e2829286b9
MukPan <109339477+MukPan@users.noreply.github.com>
2024-09-09 23:02:49 +0900

    clean: フロントエンド編集用コンポーネント"MainContent"を追加

---
+++ src/app/dashboard/MainContent.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Image from "next/image";
+
+/**
+ * メインコンテンツ
+ * */
+
+const MainContent = () => {
+  console.log("MainContent");
+
+  return (
+    <div>
+      <h1> 子供用ダック </h1>
+      <Image
+        src={"/images/duck.png"}
+        alt={"duck"}
+        width={300}
+        height={200}/>
+    </div>
+  );
+}
+
+export default MainContent;
+
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import NoticeMes from "@/utils/components/backend/NoticeMes";
 import {NoticeProvider} from "@/utils/providers/NoticeProvider";
+import MainContent from "@/app/dashboard/MainContent";
+import EmailViewer from "@/utils/components/backend/EmailViewer";
+import FCMTokenViewer from "@/utils/components/backend/FCMTokenViewer";
 
 /* -----------------------------
 
@@ -14,18 +17,14 @@ export default function Dashboard() {
 
   return (
     <NoticeProvider>
-      <h1> 子供用ダック </h1>
-      <Image
-        src={"/images/duck.png"}
-        alt={"duck"}
-        width={300}
-        height={200}/>
+      {/* フロントエンド */}
+      <MainContent />
 
       {/* バックエンド */}
       <p>/以下デバッグ用(そのうち削除予定)/</p>
       <NoticeMes/>
-      {/*<FCMTokenViewer/>*/}
-      {/*<EmailViewer/>*/}
+      <FCMTokenViewer/>
+      <EmailViewer/>
     </NoticeProvider>
   );
 }
