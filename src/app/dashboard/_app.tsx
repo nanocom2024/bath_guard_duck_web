ae020bce84fd087364b9e959f0d16c705f682203
杉浦 <130259690+X22054@users.noreply.github.com>
2024-09-18 15:27:13 +0900

    フォント変更

--- src/app/dashboard/_app.tsx
+++ src/app/dashboard/_app.tsx
@@ -2,16 +2,9 @@
 "use client";
 import { css } from "@emotion/react";
 import Image from "next/image";
-import {Button} from "@mui/material";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
+import { Button, Box } from "@mui/material";
 
-
-/**
- * メインコンテンツ
- * */
-
+// メインコンテンツ
 const MainContent = () => {
     console.log("MainContent");
 
@@ -25,25 +18,23 @@ const MainContent = () => {
     const paragraphStyle = css({
         color: "blue",
         fontSize: "20px",
+        fontFamily: "'Zen Kaku Gothic New', sans-serif", // 個別にフォントを適用
     });
 
     const headingStyle = css({
-        fontFamily: "Arial, sans-serif",
+        fontFamily: "'Zen Kaku Gothic New', sans-serif", // ヘッディングにも適用
         color: "green",
-    });
-
-    const hrStyle = css({
-        margin: "20px 0",
-        borderColor: "gray",
     });
 
     return (
         <div css={containerStyle}>
-            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
                 <p css={paragraphStyle}>おふろみまもりだっく</p>
             </Box>
             <h1 css={headingStyle}>子供用ダック</h1>
-            <Button variant="contained" sx={{ backgroundColor: "#FFFFFF", color: "black" }}>Contained</Button>
+            <Button variant="contained" sx={{ backgroundColor: "#FFFFFF", color: "black" }}>
+                Contained
+            </Button>
             <Image
                 src={"/images/duck.png"}
                 alt={"duck"}
