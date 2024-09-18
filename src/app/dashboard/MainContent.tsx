16add58678e55517b9f375834676a79a5a6ae6f6
MukPan <109339477+MukPan@users.noreply.github.com>
2024-09-18 22:42:36 +0900

    add: 状態変更のボタンを追加

--- src/app/dashboard/MainContent.tsx
+++ src/app/dashboard/MainContent.tsx
@@ -2,9 +2,6 @@
 "use client";
 import useGetLastMes from "@/utils/hooks/useGetLastMes";
 import useGetDuckState from "@/utils/hooks/useGetDuckState";
-import ChildEnter from "@/app/dashboard/ChildEnter";
-import ChildDetection from "@/app/dashboard/ChildDetection";
-import Sleep from "@/app/dashboard/Sleep";
 import {DuckState} from "@/utils/enum/DuckState";
 import {useChangeComponent} from "@/utils/hooks/useChangeComponent";
 
@@ -17,7 +14,7 @@ const MainContent = () => {
   //最新の通知からタイトルとボディを取得
   const {mesTitle, mesBody} = useGetLastMes();
   //タイトルをDBと照らし合わせてダックの状態を取得
-  const {duckState} = useGetDuckState(mesTitle);
+  const {duckState, setDuckState} = useGetDuckState(mesTitle);
   //状態によって表示するコンポーネントを変更
   const nowComponent = useChangeComponent(duckState);
 
@@ -25,6 +22,10 @@ const MainContent = () => {
     <>
       {nowComponent}
       {/*デバッグ用*/}
+      <button onClick={() => setDuckState(() => DuckState.CHILD_ENTER)}>CHILD_ENTER</button>
+      <button onClick={() => setDuckState(() => DuckState.CHILD_DETECTION)}>CHILD_DETECTION</button>
+      <button onClick={() => setDuckState(() => DuckState.SLEEP)}>SLEEP</button>
+
       {/*<div>*/}
       {/*  <h2>通知メッセージ</h2>*/}
       {/*  <h3>タイトル: {mesTitle}</h3>*/}
