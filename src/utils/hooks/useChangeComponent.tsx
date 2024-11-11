720d25fd3b389dd99c4b39acd72aedaac9e1c1ea
masaru-gif <k22089kk@aitech.ac.jp>
2024-11-11 15:25:11 +0900

    add:Senior States
    高齢者用の状態を追加しました。
    postmanでテスト済みです。

--- src/utils/hooks/useChangeComponent.tsx
+++ src/utils/hooks/useChangeComponent.tsx
@@ -1,6 +1,8 @@
+import { DuckState } from "@/utils/enum/DuckState";
 import ChildEnter from "@/app/dashboard/ChildEnter";
-import {DuckState} from "@/utils/enum/DuckState";
 import ChildDetection from "@/app/dashboard/ChildDetection";
+import SeniorEnter from "@/app/dashboard/SeniorEnter";
+import SeniorDetection from "@/app/dashboard/SeniorDetection";
 import Sleep from "@/app/dashboard/Sleep";
 
 /**
@@ -14,7 +16,11 @@ export const useChangeComponent = (duckState: DuckState) => {
     return <ChildDetection/>;
   } else if (duckState === DuckState.SLEEP) {
     return <Sleep/>;
-  } else {
+  } else if (duckState === DuckState.SENIOR_ENTER) {
+    return <SeniorEnter/>
+  } else if (duckState === DuckState.SENIOR_DETECTION) {
+    return <SeniorDetection/>
+  }else {
     return <ChildEnter/>;
   }
 }
